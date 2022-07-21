cd2265b3a60e9e19c597428c799fa5043cca83a9
Aleksey Kasmynin <kasmynin.aa@edu.spbstu.ru>
2022-07-21 17:28:58 +0300

    Pretty invalid input messages + refactor templates

--- project/forms.py
+++ project/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, SelectField
 from wtforms.validators import Regexp
 
-from randomizer import get_seed_validator
+from randomizer import get_seed_valid, SEED_LENGTH
 from rubiks_cube.slice import Slice
 from rubiks_cube_utils import rotate_choices
 
@@ -10,21 +10,25 @@ from rubiks_cube_utils import rotate_choices
 class MainPageForm(FlaskForm):
     start: SubmitField = SubmitField("Start!")
     size: SelectField = SelectField("size: ", default=3, choices=list(range(2, 6)))
-    seed: StringField = StringField("seed: ", validators=[get_seed_validator()])
+    seed: StringField = StringField("seed: ", validators=[Regexp(
+        get_seed_valid(),
+        message=f"You can only use {SEED_LENGTH} digits and/or letters in seed"
+    )])
 
 
 class RubiksCubeForm(FlaskForm):
-    small_cube_validator: Regexp = Regexp(f"(([{''.join(i.name for i in Slice if i.name.isupper())}](\'|\")?)+)?")
-    big_cube_validator: Regexp = Regexp(f"(([{''.join(i.name for i in Slice)}](\'|\")?)+)?")
+    small_cube_valid: str = ''.join(i.name for i in Slice if i.name.isupper())
+    big_cube_valid: str = ''.join(i.name for i in Slice)
 
-    sequence: StringField = StringField("Sequence: ", validators=[small_cube_validator])
+    sequence: StringField = StringField("Sequence: ")
     rotate = SelectField("Rotate: ", default="-", choices=rotate_choices.copy())
     apply: SubmitField = SubmitField("Apply!")
 
-    def __init__(self, cube_size: int | None = None, *args, **kwargs) -> None:
+    def __init__(self, cube_size: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        if cube_size is not None:
-            assert 2 <= cube_size < 6
-            self.sequence.validators = [
-                RubiksCubeForm.small_cube_validator if cube_size < 4 else RubiksCubeForm.big_cube_validator
-            ]
+        assert 2 <= cube_size < 6
+        valid: str = RubiksCubeForm.small_cube_valid if cube_size < 4 else RubiksCubeForm.big_cube_valid
+        self.sequence.validators = [Regexp(
+            f"(([{valid}](\'|\")?)+)|(^$)",
+            message=f"You can only use {', '.join(valid)} with or without a ' or \" in sequence"
+        )]
--- randomizer.py
+++ randomizer.py
@@ -1,7 +1,5 @@
 import random
 
-from wtforms.validators import Regexp
-
 from rubiks_cube_utils import alpha_digit
 
 SEED_LENGTH: int = 8
@@ -11,5 +9,5 @@ def generate_seed() -> str:
     return "".join(random.sample(alpha_digit, SEED_LENGTH))
 
 
-def get_seed_validator() -> Regexp:
-    return Regexp(r"([\w\d]{" + str(SEED_LENGTH) + "})|(^$)")
+def get_seed_valid() -> str:
+    return r"([\w\d]{" + str(SEED_LENGTH) + "})|(^$)"
